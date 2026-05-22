cf1590dbbc6eb6d87d1aa585df955ad8e1dacb70
roxane <roxanedadje83@gmail.com>
2026-05-22 11:25:41 +0000

    gitert

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-Mini-blog Flask vulnérable - Exercice d'audit de sécurité
+Mini-blog V2 Flask vulnérable - Exercice d'audit de sécurité
 ==========================================================
 ATTENTION : Ce code contient des vulnérabilités INTENTIONNELLES.
 Ne JAMAIS déployer en production.
